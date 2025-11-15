255714ff8cd0c3a81076465b9864f761700e854a
Lucky Patil <luckypatil577R@gmail.com>
2025-11-15 12:13:31 +0530

    packages installed and the web started

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(cors({credentials:true}));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:3000/`);
+
 });
