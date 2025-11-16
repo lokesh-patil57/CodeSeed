78df98640d8a5af3e1b4d4e79146846909b77aea
Lucky Patil <luckypatil577R@gmail.com>
2025-11-16 14:04:29 +0530

    server file routes and auth imported

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser"; 
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,10 +13,14 @@ app.use(express.json());
 app.use(cors({credentials:true}));
 app.use(cookieParser());
 
+
+//API endpoints
 app.get("/", (req, res) => {
   res.send("Hello World!");
 
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
