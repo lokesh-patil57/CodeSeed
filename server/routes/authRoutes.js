58a22c89cebec2a68f395eec494caf01d466062e
Lucky Patil <luckypatil577R@gmail.com>
2025-11-16 15:41:02 +0530

    routes added in verification and otp sending

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import { register, login, logout,sendVerificationOtp, verifyEmail } from "../controllers/authController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
+authRoutes.post("/send-verify-otp", userAuth, sendVerificationOtp);
+authRoutes.post("/verify-account", userAuth, verifyEmail);
 
 
 export default authRoutes;
