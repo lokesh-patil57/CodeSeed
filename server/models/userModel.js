09423da7de4a099dca7822f13b91ad891d6e6bc6
Lucky Patil <luckypatil577R@gmail.com>
2025-11-16 14:06:26 +0530

    created the User model in database

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -20,4 +20,7 @@ const userSchema = new mongoose.Schema({
   resetOtpExpiry: { type: Number, default: 0 },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model.User || mongoose.model("User", userSchema);
+ 
+
+export default User;
