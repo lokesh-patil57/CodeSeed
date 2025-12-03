03b32570f112d8964e443754cea993b27c4ebacc
Lucky Patil <luckypatil577R@gmail.com>
2025-12-03 18:04:53 +0530

    chat page and toast container added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import EmailVerify from "./pages/EmailVerify";
 import ResetPassword from "./pages/ResetPassword";
-import Login from "./pages/Login"; 
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import { ToastContainer, toast } from 'react-toastify'; 
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/email-verify" element={<EmailVerify />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </>
   );
