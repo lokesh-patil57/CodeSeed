98c447008493a4ee4f180a355856301e251b65ba
Lucky Patil <luckypatil577R@gmail.com>
2025-12-04 12:38:21 +0530

    user odel updated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,26 +1,48 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    googleId: {
+      type: String,
+      default: null,
+    },
+    verifyOtp: {
+      type: String,
+      default: "",
+    },
+    otpExpiry: {
+      type: Number,
+      default: 0,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetOtp: {
+      type: String,
+      default: "",
+    },
+    resetOtpExpiry: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  verifyOtp: { type: String, default: "" },
-  isAccountVerified: { type: Boolean, default: false },
-  resetOtp: { type: String, default: "" },
-  resetOtpExpiry: { type: Number, default: 0 },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model.User || mongoose.model("User", userSchema);
- 
 
 export default User;
