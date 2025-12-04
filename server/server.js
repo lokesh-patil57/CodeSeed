8e2721fc1f1524efc137c81228d07c33aa1fe185
Lucky Patil <luckypatil577R@gmail.com>
2025-12-04 12:36:14 +0530

    chat routes and CORS

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
-import cookieParser from "cookie-parser"; 
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
 connectDB();
 
 app.use(express.json());
-app.use(cors({credentials:true}));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 
@@ -22,9 +29,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRouter
-  
-);
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
